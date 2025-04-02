00722229e23d55d0629c1f5341ebf1067f74e952
omergrau <omergrau@gmail.com>
2025-04-02 23:57:01 +0300

    adding best score

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ def play_game(running):
     pygame.quit()
 def game_over(score):
     pygame.font.init()
-    pygame.mixer.music.load("extras\lose_video-game.mp3")
+    pygame.mixer.music.load(load_resource("extras\lose_video-game.mp3"))
     pygame.mixer.music.set_volume(0.5)
     pygame.mixer.music.play()
     if score > load_high_score():
