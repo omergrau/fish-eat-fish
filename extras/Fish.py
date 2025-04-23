a07d3b8ae07662976387380c80927a2bb0c768bd
omergrau <omergrau@gmail.com>
2025-04-23 07:39:54 +0300

    pygame init bug+load resources function

--- extras/Fish.py
+++ extras/Fish.py
@@ -1,16 +1,7 @@
 import pygame
 import random
-import os
-import sys
 from extras import resources
-def load_resource(filename):
-    if 'js' in sys.modules:
-        path = os.path.join('extras', filename)
-    elif hasattr(sys, '_MEIPASS'):
-        path = os.path.join(sys._MEIPASS, 'extras', filename)
-    else:
-        path = os.path.join(os.path.dirname(__file__), filename)
-    return pygame.image.load(path)
+
 
 class fish(pygame.sprite.Sprite):
     def __init__(self, WIDTH, HEIGHT,level):
